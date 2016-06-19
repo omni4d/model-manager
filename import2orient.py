2344bf42db9f99a1dc4d63b4e80d6398d9cfb5b2
Owen Campbell <owen.campbell@empiria.co.uk>
2016-06-19 11:03:44 +0100

    Refactor script into functions

--- import2orient.py
+++ import2orient.py
@@ -22,33 +22,45 @@ vertex_types = {
     ]
 }
 
-edge_types = ['whole', 'part', 'class', 'member']
-
-file = urlopen(
-    'https://raw.githubusercontent.com/omni4d/model/master/omni4d.core.yaml')
-model = yaml.load(file)
-
 db_name = 'omni4d'
+model_url = (
+    'https://raw.githubusercontent.com/omni4d/model/master/omni4d.core.yaml')
 
-client = OrientDB('localhost', 2424)
-client.connect('admin', 'admin')
-if not client.db_exists(db_name, STORAGE_TYPE_MEMORY):
-    client.connect('admin', 'admin')
+
+def model_from_url(url):
+    file = urlopen(url)
+    return yaml.load(file)
+
+
+def create_vertex(sign, sign_type, client):
+    client.command('create vertex %s set uuid = "%s"' % (sign_type, sign))
+
+
+def create_db(db_name, client, user='admin', password='admin'):
+    client.connect(user, password)
     client.db_create(db_name, DB_TYPE_GRAPH, STORAGE_TYPE_MEMORY)
 
-    for type, subtypes in vertex_types.items():
+    for sign_type, subtypes in vertex_types.items():
         for subtype in subtypes:
             client.command('create class %s extends V' % subtype)
 
-    for edge_type in edge_types:
-        client.command('create class %s extends E' % edge_type)
+    # for edge_type in edge_types:
+    #     client.command('create class %s extends E' % edge_type)
 
-client.db_open(db_name, 'admin', 'admin')
-for sign, attributes in model.items():
-    client.command(
-        'create vertex %s set uuid = "%s"' % (attributes['type'], sign))
-    if attributes['type'] in vertex_types['tuples']:
-        for object, details in attributes['objects'].items():
-            client.command('create edge %s from (select from V where uuid = "%s") to (select from V where uuid = "%s")' % (details['role'], sign, object))
+if __name__ == '__main__':
 
-client.db_close(db_name)
+    model = model_from_url(model_url)
+
+    client = OrientDB('localhost', 2424)
+    client.connect('admin', 'admin')
+    if not client.db_exists(db_name, STORAGE_TYPE_MEMORY):
+        create_db(db_name, client)
+
+    client.db_open(db_name, 'admin', 'admin')
+    for sign, attributes in model.items():
+        create_vertex(sign, attributes['type'], client)
+        # if attributes['type'] in vertex_types['tuples']:
+        #     for object, details in attributes['objects'].items():
+        #         print('create edge %s from (select from V where uuid = "%s") to (select from V where uuid = "%s")' % (details['role'], sign, object))
+
+    client.db_close(db_name)
