03d0f656ce015f2ed0db83369ff5f4056e5a8e7d
Owen Campbell <owen.campbell@tanti.org.uk>
2016-06-30 12:00:27 +0100

    [#5] Add logger debug lines

--- omni4d_python/sql.py
+++ omni4d_python/sql.py
@@ -6,8 +6,8 @@ logger = logging.getLogger(__name__)
 
 
 def create_db(db_name, server, port, user, password):
-    pass
+    logger.debug('sql.create_db:')
 
 
 def import_model(model, db_name, server, port, user, password):
-    pass
+    logger.debug('sql.import_model:')
